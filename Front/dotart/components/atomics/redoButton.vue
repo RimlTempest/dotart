8ba64e310059ba43acc18fa2c2ed6ce6ac764c92
zin9349 <zin9349@outlook.com>
2020-12-03 13:39:11 +0900

    追加：redoのボタンのcomponentを作成

---
+++ Front/dotart/components/atomics/redoButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <button v-on:click="clickEvent">
+    <icon-Base width="36" height="36" icon-name="redoIcon"
+      ><redoIcon
+    /></icon-Base>
+  </button>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+import { canvasDataModule } from '../../store/modules/canvasData'
+import iconBase from '@/components/atomics/icons/iconBase.vue'
+import redoIcon from '@/components/atomics/icons/redoIcon.vue'
+
+@Component({
+  middleware: 'auth',
+  components: {
+    iconBase,
+    redoIcon
+  }
+})
+export default class undoButton extends Vue {
+  @Prop({ type: Function })
+  clickEvent!: Function
+}
+</script>
+<style>
+</style>
